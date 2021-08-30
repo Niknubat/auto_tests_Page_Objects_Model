2b9809e3d89632164b4d193164d0ac5fce6a60e7
sa.tabunkin <tsagrom@gmail.com>
2021-08-31 00:24:11 +0300

    implement LoginPage.checking the presence of authorization and registration forms 4.2.8

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,16 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.ID, "#login_form")
+    LOGIN_USERNAME = (By.NAME, "login-username")
+    LOGIN_PASSWORD = (By.NAME, "login-password")
+    LOGIN_SUBMIT = (By.NAME, "login_submit")
+
+    REGISTER_FORM = (By.ID, "#register_form")
+    REGISTRATION_EMAIL = (By.NAME, "registration-email")
+    REGISTRATION_PASSWORD_1 = (By.NAME, "registration-password1")
+    REGISTRATION_PASSWORD_2 = (By.NAME, "registration-password2")
+    REGISTRATION_SUBMIT = (By.NAME, "registration_submit")
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 import time
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -15,3 +16,13 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
     time.sleep(2)
+
+
+def test_presence_authorization_and_registration_forms(browser):
+    # browser.test_guest_should_see_login_link()
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+    page = LoginPage(browser)
+    page.should_be_login_url()
